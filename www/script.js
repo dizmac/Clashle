0137eaca6a29aae75577d0b6def42169f83effc3
jchahin <jchahin11@gmail.com>
2022-04-11 20:48:33 -0400

    image button test

--- www/script.js
+++ www/script.js
@@ -1,3 +1,9 @@
+function addToDeck(){
+    console.log("addToDeck called")
+    // TODO: make this work
+}
+
+
 const get = async path => {
     const promise = new Promise((resolve, reject) => {
         fetch(path).then(x => x.json()).then(data => resolve(data));
