0a8f83c51c10656d509977da98e5468c8c3eddec
jchah <jchahin11@gmail.com>
2022-04-29 18:43:15 -0400

    - removed draggable functionality - added place card on double click

--- www/script.js
+++ www/script.js
@@ -1,54 +1,15 @@
-dragElement(document.getElementById("card"));
+let deck = [];
 
 function placeCard(card) {
-    card.style.left = "716px"
-    card.style.top = "102px"
-}
-
-function dragElement(elmnt) {
-    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    if (document.getElementById(elmnt.id + "header")) {
-        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-    } else {
-        elmnt.onmousedown = dragMouseDown;
-    }
-
-    function dragMouseDown(e) {
-        e = e || window.event
-        e.preventDefault();
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        document.onmousemove = elementDrag;
-    }
-
-    function elementDrag(e) {
-        let offsetRight = window.innerWidth - elmnt.offsetLeft - elmnt.offsetWidth;
-        e = e || window.event;
-        e.preventDefault();
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        // TODO: add bottom border when all cards are on the page
-        if(elmnt.offsetLeft < 0) {
-            elmnt.style.left = "0px";
+    if (deck.length < 8) {
+        if(deck.length < 4) {
+            card.style.top = "97px"
+            card.style.left = 716 + (deck.length * 121) + "px";
+        }else {
+            card.style.top = "248px"
+            card.style.left = 716 + ((deck.length - 4) * 121) + "px";
         }
-        if(elmnt.offsetTop < 0) {
-            elmnt.style.top = "0px";
-        }
-        if(offsetRight < 0){
-            elmnt.style.left = window.screen.width - elmnt.offsetWidth + "px";
-        }
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        // TODO: auto fit card into deck based on coords
-        console.log(`X: ${e.clientX}, Y: ${e.clientY}`)
-    }
-
-    function closeDragElement() {
-        document.onmouseup = null;
-        document.onmousemove = null;
+        deck.push(card)
     }
 }
 
